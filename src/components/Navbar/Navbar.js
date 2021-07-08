93c685594d143f3af82d1c60c2f10819e927d032
Anup <anuppoudel60@gmail.comm>
2021-07-08 12:18:46 +1000

    navbar created

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.scss"
 
 const Navbar = () => {
   return (
@@ -8,12 +10,13 @@ const Navbar = () => {
           Meals<span>App</span>
         </h1>
       </div>
-      <div className="navbar-links"></div>
+      <div className="navbar-links">
       <ul>
-        <li>Home</li>
-        <li>Categories</li>
-        <li>Random</li>
+        <Link to="/"><li>Home</li></Link>
+        <Link to="/categories"><li>Categories</li></Link>
+        <Link to="/random"><li>Random</li></Link>
       </ul>
+    </div>
     </div>
   );
 };
