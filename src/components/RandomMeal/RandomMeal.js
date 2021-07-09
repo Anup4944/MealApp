f857ccf2749772833f6a82a8198a1c82fdc1e22a
Anup <anuppoudel60@gmail.comm>
2021-07-09 22:56:48 +1000

    random page done

--- src/components/RandomMeal/RandomMeal.js
+++ src/components/RandomMeal/RandomMeal.js
@@ -1,18 +1,26 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import { myContext } from "../Context/Context";
+import "./RandomMeal.scss";
 
 const RandomMeal = () => {
-  const { randomMeal , fetchRandomMeal } = useCallback(myContext);
+  const { randomMeal, fetchRandomMeal } = useContext(myContext);
+
   useEffect(() => {
     fetchRandomMeal();
   }, [fetchRandomMeal]);
   return (
     <div className="random">
-      {randomMeal.map(meal => {
+      {randomMeal.map((meal) => {
         return (
-          <div>
-            <div></div>
-            <div></div>
+          <div className="random-grid" key={meal.idMeal}>
+            <div className="random-grid-controls">
+              <img src={meal.strMealThumb} alt="#" />
+              <button onClick={fetchRandomMeal}>Get another meal</button>
+            </div>
+            <div className="random-grid-instruction">
+              <h4>Instructions</h4>
+              <p>{meal.strInstructions}</p>
+            </div>
           </div>
         );
       })}
